31a9925191a3e7f58e2bb8429b912b17938558c7
manel <manelbng@gmail.com>
2024-09-11 17:44:19 +0200

    extra

--- src/rag_book_reviews/tools/custom_tool.py
+++ src/rag_book_reviews/tools/custom_tool.py
@@ -1,12 +1,37 @@
 from crewai_tools import BaseTool
 
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import DeepLake
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-class MyCustomTool(BaseTool):
-    name: str = "Name of my tool"
+class BookRAGTool(BaseTool):
+    name: str = "RAG tool"
     description: str = (
-        "Clear description for what this tool is useful for, you agent will need this information to use it."
+        "Allows the agent to search and store for book reviews in a vector database."
     )
+    def __init__(self, activeloop_id, dataset_name):
+        self.embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
+        self.dataset_path = f"hub://{activeloop_id}/{dataset_name}"
+        self.db = DeepLake(dataset_path=self.dataset_path, embedding_function=self.embeddings)
+        self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+        self.llm = OpenAI(model_name="gpt-3.5-turbo-instruct", temperature=0)
+        self.chain = RetrievalQAWithSourcesChain.from_chain_type(
+            llm=self.llm,
+            chain_type="stuff",
+            retriever=self.db.as_retriever()
+        )
 
-    def _run(self, argument: str) -> str:
-        # Implementation goes here
-        return "this is an example of a tool output, ignore it and move along."
+    def add_opinions(self, opinions):
+        all_texts, all_metadatas = [], []
+        for opinion in opinions:
+            chunks = self.text_splitter.split_text(opinion["text"])
+            for chunk in chunks:
+                all_texts.append(chunk)
+                all_metadatas.append({"source": opinion["source"]})
+        self.db.add_texts(all_texts, all_metadatas)
+
+    def get_response(self, question):
+        response = self.chain({"question": question})
+        return response
